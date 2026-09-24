59a48664fefb94453584508a91b2bd5ffabffd46
VKLF-OFFICIAL <vklf_oficial@proton.me>
2026-09-24 14:18:25 +0200

    Ejercicio 7

---
+++ TiposDeDatosSimples/Númericos/Ejercicio 7.py
@@ -0,0 +1,5 @@
+peso = float(input("Ingresa tu peso >> "))
+estatura = float(input("Ingresa tu estatura en metros >> "))
+imc = round(peso / estatura)
+
+print(f"Tu indice de masa corporal es {imc} .")
