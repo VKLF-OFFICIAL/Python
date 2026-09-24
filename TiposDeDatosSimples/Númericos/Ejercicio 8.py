c2a969a5224880799babfda9a90e2bdef3a1ff1e
VKLF-OFFICIAL <vklf_oficial@proton.me>
2026-09-24 14:29:40 +0200

    Ejercicio 8

---
+++ TiposDeDatosSimples/Númericos/Ejercicio 8.py
@@ -0,0 +1,7 @@
+n = int(input("Ingresa el número entero 1 >> "))
+m = int(input("Ingresa el número entero 2 >> "))
+
+c = n // m
+r = n % m
+
+print(f"El resultado de {n} entre {m} da un cociente {c} y un resto {r}")
