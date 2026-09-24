5e1bab0025ea1785dbf70fc538ebe2df24f4d645
VKLF-OFFICIAL <vklf_oficial@proton.me>
2026-09-24 13:52:33 +0200

    Ejercicio 5

---
+++ TiposDeDatosSimples/Númericos/Ejercicio5.py
@@ -0,0 +1,5 @@
+horas = int(input("¿Cuantas horas has trabajado?>"))
+precioH = int(input("¿Cuanto cobras la hora ?>"))
+salario = horas * precioH
+
+print(f"Debes cobrar en total " + str(salario) + " ." )
